1692dee018ed658251a255a1c335cf3d62d8c05e
Jonlenes Silva Castro <jonlenes.castro@minerva.local>
2020-11-14 13:58:33 -0300

    Adding get_gurobi_model to util

--- src/util.py
+++ src/util.py
@@ -1,4 +1,18 @@
 import json
+import gurobipy as gp
+
+def get_gurobi_model(timeout=None, verbose=0, seed=42, name='smpp'):
+    # Create a new model
+    model = gp.Model(name)
+
+    # Set Params
+    model.setParam(gp.GRB.Param.OutputFlag, verbose)
+    model.setParam(gp.GRB.Param.Seed, seed)
+
+    if timeout:
+        model.setParam(gp.GRB.Param.TimeLimit, timeout)
+
+    return model
 
 def read_json(filename):
     with open(filename, "r") as file:
